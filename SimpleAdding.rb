d2e57fdf2ae9171d2014aa928fce10079f900c30
forcrson <forceson23@gmail.com>
2015-07-31 10:27:51 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,14 @@
+def SimpleAdding(num)
+
+  a=0
+  for i in 1..num
+  a=i+a
+  end
+  # code goes here
+  return a 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)
